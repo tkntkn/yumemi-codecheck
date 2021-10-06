aabaa869e4df887344578efaee37d4ff61cafc9a
tkntkn <kd1049@gmail.com>
2021-10-06 18:50:06 +0900

    Run Prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
-import ReactDOM from 'react-dom';
+import { useState } from "react";
+import ReactDOM from "react-dom";
 import "./main.scss";
 
 function App() {
-	const [count, setCount] = useState(0);
-	return (
-		<div className="app">
-			<div className="app__count">{count}</div>
-			<button className="app__countButton" onClick={() => setCount(count => ++count)}>Count Up</button>
-		</div>
-	);
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <div className="app__count">{count}</div>
+      <button
+        className="app__countButton"
+        onClick={() => setCount((count) => ++count)}
+      >
+        Count Up
+      </button>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,33 @@
-const path = require('path');
-const webpack = require('webpack');
-require('dotenv').config();
+const path = require("path");
+const webpack = require("webpack");
+require("dotenv").config();
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main.tsx',
+  mode: "development",
+  entry: "./src/main.tsx",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ]
-      }
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new webpack.DefinePlugin({
-      RESAS_API_KEY: JSON.stringify(process.env.RESAS_API_KEY), 
-    })
-  ]
+      RESAS_API_KEY: JSON.stringify(process.env.RESAS_API_KEY),
+    }),
+  ],
 };
